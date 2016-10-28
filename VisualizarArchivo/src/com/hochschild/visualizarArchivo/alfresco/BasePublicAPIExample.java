b9762a3bb32bd7ce5af3f7f6c4362d36d6de708e
segundo.leiva <segundo.leiva@PE-L00425.hundred.com.pe>
2016-10-28 16:06:14 -0500

    ed

--- VisualizarArchivo/src/com/hochschild/visualizarArchivo/alfresco/BasePublicAPIExample.java
+++ VisualizarArchivo/src/com/hochschild/visualizarArchivo/alfresco/BasePublicAPIExample.java
@@ -1,27 +1,13 @@
 package com.hochschild.visualizarArchivo.alfresco;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.chemistry.opencmis.client.api.CmisObject;
-import org.apache.chemistry.opencmis.client.api.Document;
-import org.apache.chemistry.opencmis.client.api.Folder;
-import org.apache.chemistry.opencmis.client.api.ItemIterable;
 import org.apache.chemistry.opencmis.client.api.Session;
-import org.apache.chemistry.opencmis.commons.data.ContentStream;
-import org.apache.chemistry.opencmis.commons.exceptions.CmisContentAlreadyExistsException;
-import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
 
 import com.alfresco.api.example.model.ContainerEntry;
 import com.alfresco.api.example.model.ContainerList;
 import com.alfresco.api.example.model.NetworkEntry;
 import com.alfresco.api.example.model.NetworkList;
-import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpContent;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 
@@ -35,43 +21,9 @@ import com.google.api.client.http.HttpRequestFactory;
  */
 abstract public class BasePublicAPIExample {
 	public static final String SITES_URL = "/public/alfresco/versions/1/sites/";
-	public static final String NODES_URL = "/public/alfresco/versions/1/nodes/";
 
 	private String homeNetwork;
-	
-	/**
-	 * Use the CMIS API to get a handle to the root folder of the
-     * target site, then create a new folder, then create
-     * a new document in the new folder
-     * 
-	 * @param cmisSession
-	 * @param parentFolderId
-	 * @param folderName
-	 * @return Folder
-	 * 
-	 * @author jpotts
-	 * 
-	 */
-	public Folder createFolder(String parentFolderId, String folderName) {
-		Session cmisSession = getCmisSession();        
-        Folder rootFolder = (Folder) cmisSession.getObject(parentFolderId);
-        
-        Folder subFolder = null;
-        try {
-        	// Making an assumption here that you probably wouldn't normally do
-        	subFolder = (Folder) cmisSession.getObjectByPath(rootFolder.getPath() + "/" + folderName);
-        	System.out.println("Folder already existed!");
-        } catch (CmisObjectNotFoundException onfe) {
-            Map<String, Object> props = new HashMap<String, Object>();
-            props.put("cmis:objectTypeId",  "cmis:folder");
-            props.put("cmis:name", folderName);
-            subFolder = rootFolder.createFolder(props);
-            String subFolderId = subFolder.getId();
-            System.out.println("Created new folder: " + subFolderId);        	
-        }        
-        
-        return subFolder;
-	}
+
 
 	public String getHomeNetwork() throws IOException {
 		if (this.homeNetwork == null) {
@@ -94,160 +46,6 @@ abstract public class BasePublicAPIExample {
 	        System.out.println("Your home network appears to be: " + homeNetwork);
 		}
 	    return this.homeNetwork;
-	}
-
-	public Document createDocument(String idArchivo, String idSolicitud,Folder parentFolder,
-								   File file,
-								   String fileType)
-			throws FileNotFoundException {
-		return createDocument(idArchivo, idSolicitud, parentFolder, file, fileType, null);
-	}
-
-	/**
-	 * Use the CMIS API to create a document in a folder
-	 * 
-	 * @param cmisSession
-	 * @param parentFolder
-	 * @param file
-	 * @param fileType
-	 * @param props
-	 * @return
-	 * @throws FileNotFoundException
-	 * 
-	 * @author jpotts
-	 * 
-	 */
-	public Document createDocument(String idArchivo, String idSolicitud, Folder parentFolder,
-								   File file,
-								   String fileType,
-								   Map<String, Object> props)
-			throws FileNotFoundException {
-
-		Session cmisSession = getCmisSession();
-		
-		String fileName = idSolicitud+"-"+idArchivo+"."+file.getName().split("\\.")[1];
-
-		// create a map of properties if one wasn't passed in
-		if (props == null) {
-			props = new HashMap<String, Object>();
-		}
-
-		// Add the object type ID if it wasn't already
-		if (props.get("cmis:objectTypeId") == null) {
-			props.put("cmis:objectTypeId",  "cmis:document");
-		}
-		
-		// Add the name if it wasn't already
-		if (props.get("cmis:name") == null) {
-			props.put("cmis:name", fileName);
-		}
-
-		ContentStream contentStream = cmisSession.getObjectFactory().
-				  createContentStream(
-					fileName,
-					file.length(),
-					fileType,
-					new FileInputStream(file)
-				  );
-
-		Document document = null;
-		try {
-			ItemIterable<CmisObject> children = parentFolder.getChildren();
-			for (CmisObject o : children) {
-				if(o.getName().equals(idSolicitud+"-"+idArchivo)){
-					o.delete();
-				}
-			}
-			
-			document = parentFolder.createDocument(props, contentStream, null);		
-			System.out.println("Created new document: " + document.getId());
-		} catch (CmisContentAlreadyExistsException ccaee) {
-			document = (Document) cmisSession.getObjectByPath(parentFolder.getPath() + "/" + fileName);
-			System.out.println("Document already exists: " + fileName);
-		}
-		
-		return document;		
-	}
-	
-	public void eliminarDocumento(String idArchivo, String idSolicitud,
-			Folder parentFolder){
-
-			ItemIterable<CmisObject> children = parentFolder.getChildren();
-			for (CmisObject o : children) {
-				if (o.getName().contains(idSolicitud + "-" + idArchivo)) {
-					o.delete();
-				}
-			}
-	}
-
-	/**
-	 * Use the REST API to find the documentLibrary folder for
-     * the target site
-	 * @return String
-	 * 
-	 * @author jpotts
-	 * 
-	 */
-	public String getRootFolderId(String site) throws IOException {
-		
-        GenericUrl containersUrl = new GenericUrl(getAlfrescoAPIUrl() +
-                                             getHomeNetwork() +
-        		                             SITES_URL +
-                                             site +
-                                             "/containers");
-        System.out.println(containersUrl);
-        HttpRequest request = getRequestFactory().buildGetRequest(containersUrl);
-        ContainerList containerList = request.execute().parseAs(ContainerList.class);
-        String rootFolderId = null;
-        for (ContainerEntry containerEntry : containerList.list.entries) {
-        		if (containerEntry.entry.folderId.equals("documentLibrary")) {
-        			rootFolderId = containerEntry.entry.id;
-        			break;
-        		}
-        }
-        return rootFolderId;
-	}
-
-	/**
-	 * Use the REST API to "like" an object
-	 * 
-	 * @param requestFactory
-	 * @param homeNetwork
-	 * @param objectId
-	 * @throws IOException
-	 */
-	public void like(String objectId) throws IOException {
-        GenericUrl likeUrl = new GenericUrl(getAlfrescoAPIUrl() + 
-        					 getHomeNetwork() +
-        					 NODES_URL + 
-        					 objectId +
-        					 "/ratings");
-        HttpContent body = new ByteArrayContent("application/json", "{\"id\": \"likes\", \"myRating\": true}".getBytes());
-        HttpRequest request = getRequestFactory().buildPostRequest(likeUrl, body);
-        request.execute();
-        System.out.println("You liked: " + objectId);
-	}
-	
-	/**
-	 * Use the REST API to comment on an object
-	 * 
-	 * @param requestFactory
-	 * @param homeNetwork
-	 * @param objectId
-	 * @param comment
-	 * @throws IOException
-	 */
-	public void comment(String objectId, String comment) throws IOException {
-        GenericUrl commentUrl = new GenericUrl(getAlfrescoAPIUrl() + 
-        					 getHomeNetwork() +
-        					 NODES_URL + 
-        					 objectId +
-        					 "/comments");
-        HttpContent body = new ByteArrayContent("application/json",
-        										("{\"content\": \"" + comment + "\"}").getBytes());
-        HttpRequest request = getRequestFactory().buildPostRequest(commentUrl, body);
-        request.execute();
-        System.out.println("You commented on: " + objectId);
 	}
 
 	abstract public String getAlfrescoAPIUrl();	
